9eb94ce9d0d0c74c19f773edfec9dca368b94787
Matheus Souza Rozendo <matheusrozendo234674@gmail.com>
2022-08-13 14:47:18 -0300

    Update Arquivo_TXT_de_Saida.h

--- classes/Arquivo_TXT_de_Saida.h
+++ classes/Arquivo_TXT_de_Saida.h
@@ -1,25 +1,24 @@
 #ifndef ARQUIVO_TXT_DE_SAIDA_H
 #define ARQUIVO_TXT_DE_SAIDA_H
 
-
 #include <bits/stdc++.h>
 #include "Arvore.h"
 #include "Arquivo.h"
 #include "leitura_dados.h"
 
 
-class Arquivo_TXT_de_Saida : protected Arquivo
+class Arquivo_TXT_de_Saida : public Arquivo
 {
     public:
         Arquivo_TXT_de_Saida();
-        bool cria_arquivo(string n, string p,string extensao); // set do arquivo de saida o parametro não precisa conter .txt
+        bool cria_arquivo(string nome, string pasta,string extensao); // set do arquivo de saida o parametro nÃ£o precisa conter .txt
         ofstream *Get_arquivo(); // retorna um ponteiro para o arquivo de saida
 
 
     private:
-        string pasta;//contem a pasta onde o arquivo de saida será criado
+        string pasta;//contem a pasta onde o arquivo de saida serÃ¡ criado
         ofstream arquivo; // contem o arquivo de saida
-        void cria_pasta(); // se a pasta definida como destino não existir, cria uma pasta com o nome passado
+        void cria_pasta(); // se a pasta definida como destino nÃ£o existir, cria uma pasta com o nome passado
 };
 
 Arquivo_TXT_de_Saida::Arquivo_TXT_de_Saida(){};
@@ -34,26 +33,23 @@ void Arquivo_TXT_de_Saida::cria_pasta(){
     }
 }
 
-bool Arquivo_TXT_de_Saida::cria_arquivo(string n, string p, string extensao){
+bool Arquivo_TXT_de_Saida::cria_arquivo(string nome, string pasta, string extensao){
     string aux;
-    aux = n;
-    aux+=".txt";
-    Set_Caminho(n);
-    pasta=p;
+    aux = nome;
+    aux+=extensao;
+    Set_Caminho(nome);
+    this->pasta=pasta;
     cria_pasta();
     aux = pasta;
     aux+="/";
-    aux+=n;
+    aux+=nome;
     aux+=extensao;
     arquivo.open(aux);
-    if(arquivo.is_open()){
-        return true;
-    }else{
-        return false;
-    }
+    return (arquivo.is_open())?true : false;
 }
 
 ofstream *Arquivo_TXT_de_Saida::Get_arquivo(){
     return &arquivo;
 }
+
 #endif // ARQUIVO_TXT_DE_SAIDA_H
